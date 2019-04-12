e24ff8c240bb003600d1935d7fcb25ad223fb2af
Denise <ddplocher@gmail.com>
2019-04-12 10:59:14 -0500

    set up

--- user-signup/main.py
+++ user-signup/main.py
@@ -3,8 +3,14 @@ from flask import Flask, request
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+
+
+
+
 form = """
 <!doctype html>
+
+<h1><strong> User-Signup<strong></h1>
 <html>
     <body>
         <form action ="/welcome">
@@ -18,15 +24,56 @@ form = """
     
 
 
+""" 
+
+password = """
+<style>
+    .error{{color: purple;}}
+</style>
+
+<form method = 'POST'>
+    <label> Password 
+        <input name= "Password" type = "text"/>
+</label>
+<input type= "submit" value= "Submit"/>
+</form> 
+"""
+
+verify_password = """
+ <style>
+    .error{{color: purple;}}
+</style>
+
+<form method = 'POST'>
+    <label> Verify Password 
+        <input name= "Verify Passord" type = "text"/>
+</label>
+<input type= "submit" value= "Submit"/>
+</form> 
+"""
+
+email_address = """
+<style>
+    .error{{color: purple;}}
+</style>
+
+<form method = 'POST'>
+    <label> Email Address (Optional) 
+        <input name= "Email Address" type = "text"/>
+</label>
+<input type= "submit" value= "Submit"/>
+</form> 
 """
 
 
 @app.route("/")
 def index():
-    return form
+
+    content = form + password + verify_password + email_address
+    return content
 
 @app.route("/welcome")
 def welcome():
     username = request.args.get("username")
-    return '<h1>Hello ' + str(username) +'</h1>'
+    return '<h1>Welcome ' + str(username) +'!</h1>'
 app.run()
