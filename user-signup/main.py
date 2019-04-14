0d60e15f0b3fdfffd8e41eb94833a98774c03f9e
Denise <ddplocher@gmail.com>
2019-04-13 20:00:58 -0500

    login works

--- user-signup/main.py
+++ user-signup/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, redirect
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -15,12 +15,12 @@ form = """
 <h1><strong> Signup <strong></h1>
 <html>
     <body>
-        <form action = "/">
+        <form method= 'POST'>
         <label class= "label" for = "username">Username</label>
             <input type = "text" name= "username" value ='{username}'/> 
         </label>
     <p class= 'error'>{username_error}</p>
-<br>
+
     
 
     <label> Password 
@@ -28,20 +28,20 @@ form = """
     </label>
     <p class = "error">{password_error}</p>
 
-<br>
+
 
     <label> Verify Password 
-        <input name= "verify_password" type = "password" value ='{password}'/>
+        <input name= "verify_password" type = "password" value ='{verify_password}'/>
     </label>
     <p class = "error">{verify_password_error}</p>
-<br>
+
 
     <label> Email Address (Optional) 
-        <input name= "email_address" type = "text" value ='{email_address}'/>
+        <input name= "email_address" type = "text" value ='{email_address}'/><p class = "error"> {email_address_error}</p>
+    
     </label>
-    <p class = "error"> {email_address_error}</p>
 
-<br>
+
 
 <input type= "submit" value= "Submit"/>
 </body>
@@ -50,50 +50,97 @@ form = """
 </html>
 """
 
-@app.route("/", methods=['POST'])
+@app.route("/validate_form")
 def display_form():
-    return form.format(username='' ,username_error= '', password= '', password_error= '', verify_password= '', verify_password_error= '', email_address = '', email_address_error= '')
+    return form.format(username='' ,username_error= '', password='', password_error='', verify_password='', verify_password_error='',email_address='', email_address_error='')
 
 def input_length(input):
-    if len(input) > 3 and len(input) < 20:
+    if len(str(input)) > 2 and len(str(input)) < 19:
         return True
     else:
         return False
 
-def check_for_space(input):
-    if i in range(len(input)) == '':
-        return False
-    else:
-        return True
 
-@app.route("/", methods=['POST'])
+#not working, fix
+def check_for_space(name): 
+    new_name = name.replace(" ", "MM")
+    if len(new_name) == len(name):
+        return True
+    else: 
+        return False
+            
+            
+def password_check(password, verify_password):
+    if str(password) == str(verify_password):
+        return True
+    else:
+        return False
+
+def check_email(email_address):
+    new_email_address = email_address.replace ("@", "")
+    end_email_address = new_email_address.replace(".", "")
+    if len(end_email_address) == len(email_address)-2:
+        return True
+    else:
+        return False
+           
+
+@app.route("/validate_form", methods=['POST'])
 def validate_form():
 
     username = request.form['username']
-    password = request.form['password']
-    verify_password = request.form['verify_password']
-    email_address = request.form['email_address']
-
     username_error = ''
+    password = request.form['password']
     password_error = ''
+    verify_password = request.form['verify_password']
     verify_password_error = ''
-    email_address_errror = ''
+    email_address = request.form['email_address']
+    email_address_error = ''
 
-    if not input_length(username):
-        username_error = 'Not a valid username'
-
+    if not check_for_space(email_address):
+        email_address_error = 'Not valid email address (spaces)'
+    else: 
+        if not check_email(email_address):
+            email_address_error = 'Not valid email address(must have "@" and ".")'
+    
+    if not input_length(email_address):
+            email_address_error = 'Not valid email length (3-20 charectors)'
+    
+    if not check_for_space(username):
+        username_error = 'Not valid username (spaces)'
+    else:
+        if not input_length(username):
+            username_error = 'Not valid username length (3-20 charectors)'
+    
     if not input_length(password):
-        password_error = 'Not a valid password'
+        password_error = 'Not valid password length (3-20 charectors)'
+    else:
+        if not check_for_space(password):
+            password_error = 'Not valid password (spaces)'
 
     if not input_length(verify_password):
-        validate_password_error = 'not valid password'
-    if not username_error and not password_error and not validate_password_error:
-        return SUCCESS
+        verify_password_error = 'Not valid password length (3-20 charectors)'
+    
+    if not password_check(password, verify_password):
+        password_error = 'password and verify password did not match'
+        verify_password_error = 'password and verify password did not match'
+
+    if not username_error and not password_error and not verify_password_error:
+        
+        return redirect('/valid_login')
     else:
-            return form.format(username_error=username_error, password_error = password_error, validate_password_error= validate_password_error)
+        return form.format(email_address_error= email_address_error, email_address= email_address, password= password, password_error= password_error, verify_password= verify_password, verify_password_error= verify_password_error, username_error= username_error, username=username)
+    
 
 
+@app.route('/valid_login')
+def valid_login():
+    
+    return '<h1> Welcome</h1>'
 
 
+               
+    
+    
 
 app.run()
